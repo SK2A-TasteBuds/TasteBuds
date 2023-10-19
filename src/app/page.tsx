e055e5a5b3529240d460005e16b8aa74b89286cb
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-10-19 12:04:03 +0900

    tailwind config に色の変数を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-5xl">Hello Main Page!</h1>
+    <main className="flex min-h-screen flex-col items-center  p-24">
+      <h1 className="text-5xl bg-orangePrimary text-white">Primary</h1>
+      <h2 className="text-3xl bg-orangeSecondary text-black">Secondary</h2>
+      <h3 className="text-xl bg-orangeAccent text-black">Accent</h3>
     </main>
   );
 }
