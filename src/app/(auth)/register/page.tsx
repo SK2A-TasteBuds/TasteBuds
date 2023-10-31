1c6442e3cc05089cf6f1c2291582cb1392be68b5
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-10-31 23:24:20 +0900

    TODO register form

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,5 +1,13 @@
 const Register = () => {
-  return <div>Register Up page</div>;
+  return (<>
+  <h1>Register Page</h1>
+  <div className="relative border-2 border-black p-8 m-4 rounded-md">
+    <form>
+      <div className="absolute top-2 right-2">1/2</div>
+    </form>
+  </div>
+  </>
+  );
 };
 
 export default Register;
