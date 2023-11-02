b6ec6bde5a30fe7c5ff9bbd96c23f44ce836f6e8
SFjapan <fs12265669@gmail.com>
2023-11-02 09:35:43 +0900

    main画面作成

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -7,8 +7,8 @@ export default function Main() {
         <div className="main flex flex-col overflow-hidden items-center w-full h-screen">
             <div className="store-img relative h-3/5 w-4/5  overflow-hidden">
                 <div className="swipe flex ">
-                    <div className="object-cover block h-full w-[20px] bg-pink1 rounded-md absolute z-10 left-[245px]"></div>
-                    <div className="object-cover block h-full w-[20px] bg-pink2 rounded-md absolute z-10 left-[235px]"></div>
+                    <div className="object-cover block h-full w-[20px] bg-[#ffc7b7] rounded-md absolute z-10 left-[245px]"></div>
+                    <div className="object-cover block h-full w-[20px] bg-[#fe9477] rounded-md absolute z-10 left-[235px]"></div>
                     <img src="https://crea.ismcdn.jp/mwimgs/9/3/1200wm/img_93abdd5347237682a14cf050f2e2a6bd74395.jpg" alt="店舗画像" className="object-cover w-4/5 h-full absolute z-10 rounded-md" />
 
                 </div>
