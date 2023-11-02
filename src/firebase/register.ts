1369dd15021445a75d575054e258faa3604b1e7e
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-11-03 05:47:24 +0900

    TODO : store user in db

--- src/firebase/register.ts
+++ src/firebase/register.ts
@@ -1,7 +1,10 @@
 import { auth, db } from "@/firebase/configs";
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { collection } from "firebase/firestore";
 
 export const signUp = async (email: string, password: string) => {
   const new_user = await createUserWithEmailAndPassword(auth, email, password);
   console.log(new_user);
+  //create new user in db
+  const usersRef = collection(db, "users");
 };
