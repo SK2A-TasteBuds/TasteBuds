d9743bbd4cad4b33d429e06109f9a7deabb91402
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-11-09 10:26:51 +0900

    add genre route

--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -12,75 +12,75 @@ export default async function Main() {
     redirect("/getting-started");
   }
   return (
-    <div className='main flex flex-col overflow-hidden items-center w-full h-screen'>
-      <div className='store-img relative h-3/5 w-4/5  overflow-hidden'>
-        <div className='swipe flex '>
-          <div className='object-cover block h-full w-[20px] bg-[#ffc7b7] rounded-md absolute z-10 left-[245px]'></div>
-          <div className='object-cover block h-full w-[20px] bg-[#fe9477] rounded-md absolute z-10 left-[235px]'></div>
+    <div className="main flex flex-col overflow-hidden items-center w-full h-screen">
+      <div className="store-img relative h-3/5 w-4/5  overflow-hidden">
+        <div className="swipe flex ">
+          <div className="object-cover block h-full w-[20px] bg-[#ffc7b7] rounded-md absolute z-10 left-[245px]"></div>
+          <div className="object-cover block h-full w-[20px] bg-[#fe9477] rounded-md absolute z-10 left-[235px]"></div>
           <img
-            src='https://crea.ismcdn.jp/mwimgs/9/3/1200wm/img_93abdd5347237682a14cf050f2e2a6bd74395.jpg'
-            alt='店舗画像'
-            className='object-cover w-4/5 h-full absolute z-10 rounded-md'
+            src="https://crea.ismcdn.jp/mwimgs/9/3/1200wm/img_93abdd5347237682a14cf050f2e2a6bd74395.jpg"
+            alt="店舗画像"
+            className="object-cover w-4/5 h-full absolute z-10 rounded-md"
           />
         </div>
 
-        <div className='absolute bottom-20 left-10  p-2 bg-transparent z-20'>
-          <p className='bg-transparent'>ECCパフェ</p>
-          <p className='bg-transparent'>中崎町</p>
+        <div className="absolute bottom-20 left-10  p-2 bg-transparent z-20">
+          <p className="bg-transparent">ECCパフェ</p>
+          <p className="bg-transparent">中崎町</p>
         </div>
       </div>
 
-      <div className='good-bad flex flex-row h-10 justify-center'>
-        <button className='mr-20'>
+      <div className="good-bad flex flex-row h-10 justify-center">
+        <button className="mr-20">
           <img
-            src='https://www.svgrepo.com/show/421162/bad-dislike-thumbs-down.svg'
-            alt='good'
-            className=' object-cover w-full h-full'
+            src="https://www.svgrepo.com/show/421162/bad-dislike-thumbs-down.svg"
+            alt="good"
+            className=" object-cover w-full h-full"
           />
         </button>
-        <button className=''>
+        <button className="">
           <img
-            src='https://www.svgrepo.com/show/478767/good-job-hand-2.svg'
-            alt='good'
-            className='object-cover w-full h-full'
+            src="https://www.svgrepo.com/show/478767/good-job-hand-2.svg"
+            alt="good"
+            className="object-cover w-full h-full"
           />
         </button>
       </div>
 
-      <div className='under-menu-bar flex flex-row bg-gray'>
-        <div className='w-1/5'>
+      <div className="under-menu-bar flex flex-row bg-gray">
+        <div className="w-1/5">
           <img
-            src='https://www.svgrepo.com/show/527754/home-1.svg'
-            alt=''
-            className='w-full h-auto'
+            src="https://www.svgrepo.com/show/527754/home-1.svg"
+            alt=""
+            className="w-full h-auto"
           />
         </div>
-        <div className='w-1/5'>
+        <div className="w-1/5">
           <img
-            src='https://www.svgrepo.com/show/527754/home-1.svg'
-            alt=''
-            className='w-full h-auto'
+            src="https://www.svgrepo.com/show/527754/home-1.svg"
+            alt=""
+            className="w-full h-auto"
           />
         </div>
-        <div className='w-1/5'>
+        <div className="w-1/5">
           <img
-            src='https://www.svgrepo.com/show/527754/home-1.svg'
-            alt=''
-            className='w-full h-auto'
+            src="https://www.svgrepo.com/show/527754/home-1.svg"
+            alt=""
+            className="w-full h-auto"
           />
         </div>
-        <div className='w-1/5'>
+        <div className="w-1/5">
           <img
-            src='https://www.svgrepo.com/show/527754/home-1.svg'
-            alt=''
-            className='w-full h-auto'
+            src="https://www.svgrepo.com/show/527754/home-1.svg"
+            alt=""
+            className="w-full h-auto"
           />
         </div>
-        <div className='w-1/5'>
+        <div className="w-1/5">
           <img
-            src='https://www.svgrepo.com/show/527754/home-1.svg'
-            alt=''
-            className='w-full h-auto'
+            src="https://www.svgrepo.com/show/527754/home-1.svg"
+            alt=""
+            className="w-full h-auto"
           />
         </div>
       </div>
