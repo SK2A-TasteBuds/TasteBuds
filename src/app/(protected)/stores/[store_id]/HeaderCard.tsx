4cdab2819e15d0204d3833c5a66ab24630afab7b
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-11-17 00:28:42 +0900

    add data fetching in store

--- src/app/(protected)/stores/[store_id]/HeaderCard.tsx
+++ src/app/(protected)/stores/[store_id]/HeaderCard.tsx
@@ -1,15 +1,26 @@
-import React from "react";
+import Image from "next/image";
 
-function Headercard() {
+async function Headercard({ data }: { data: Promise<Response> }) {
+  const store = await data.then((res) => res.json());
+  console.log(store.photo);
+  const photo = store.photo;
   return (
     <div>
-      <p className=''>Header card</p>
-      <div className='bg-slate-400 w-full'>
-        <img
-          src='https://images.unsplash.com/photo-1682685797140-c17807f8f217'
-          alt='header image'
-          className='max-w-sm w-full rounded-lg'
+      <div className=' flex flex-col md:flex-row items-center justify-center bg-blue-100  px-2 py-4'>
+        <Image
+          src={photo}
+          alt={store.name}
+          height={320}
+          width={320}
+          priority={true}
+          className='rounded-lg '
         />
+        <div className='w-full'>
+          <p>{store.name}</p>
+          <p>{store.catch}</p>
+          <p>{store.genre.name}</p>
+          <p>{store.budget.name}</p>
+        </div>
       </div>
     </div>
   );
--- src/app/(protected)/stores/[store_id]/page.tsx
+++ src/app/(protected)/stores/[store_id]/page.tsx
@@ -1,14 +1,11 @@
-import HeaderCard from "./Headercard";
+import HeaderCard from "./HeaderCard";
 import ReviewGridItem from "./ReviewGrid";
 import ReviewItem from "./Reviews";
 export default function Page({ params }: { params: { store_id: string } }) {
   const { store_id } = params;
   return (
     <>
-      store id {store_id}
-      <HeaderCard />
-      <ReviewGridItem />
-      <ReviewItem />
+      <HeaderCard data={fetch(`http://localhost:3000/api/stores/${store_id}`)} />
     </>
   );
 }
