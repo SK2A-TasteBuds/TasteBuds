2d93f3d89dc186b83e4f7c8c56aa57eaa9982c30
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-11-17 11:02:11 +0900

    add badge

--- src/app/(protected)/stores/[store_id]/HeaderCard.tsx
+++ src/app/(protected)/stores/[store_id]/HeaderCard.tsx
@@ -39,7 +39,21 @@ async function HeaderCard_2({ data }: { data: Promise<Response> }) {
         alt="Product Image"
       />
       <div className="p-4 m-1">
-        <h3 className="text-xs">{store.genre.name}</h3>
+        <div className="flex justify-between">
+          <h3 className="text-xs">{store.genre.name}</h3>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6 fill-orange-500"
+          >
+            <path
+              fillRule="evenodd"
+              d="M12.963 2.286a.75.75 0 00-1.071-.136 9.742 9.742 0 00-3.539 6.177A7.547 7.547 0 016.648 6.61a.75.75 0 00-1.152-.082A9 9 0 1015.68 4.534a7.46 7.46 0 01-2.717-2.248zM15.75 14.25a3.75 3.75 0 11-7.313-1.172c.628.465 1.35.81 2.133 1a5.99 5.99 0 011.925-3.545 3.75 3.75 0 013.255 3.717z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
         <h2 className=" text-lg font-bold text-gray-900">{store.name}</h2>
         <p className=" text-sm text-gray-700 py-1">{store.catch}</p>
         <div className="flex py-1">
@@ -61,7 +75,7 @@ async function HeaderCard_2({ data }: { data: Promise<Response> }) {
         </div>
 
         <p className="text-xs font-light text-gray-400 pl-1">
-          {store.budget.average}
+          平均: {store.budget.average}
         </p>
         <div className="flex py-2">
           <svg
