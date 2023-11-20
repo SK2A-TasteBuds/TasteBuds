1f663c6d0a45371b00610e114421ac05aafd3bc8
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-11-20 10:57:38 +0900

    change grid style

--- src/app/(protected)/stores/[store_id]/ReviewGrid.tsx
+++ src/app/(protected)/stores/[store_id]/ReviewGrid.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 function ReviewGridItem() {
   return (
-    <div className="mx-auto  max-w-lg transform overflow-hidden  bg-white shadow-md duration-300 hover:scale-105 hover:shadow-lg">
+    <div className="mx-auto  max-w-lg w-full transform overflow-hidden  bg-white shadow-md duration-300 hover:scale-105 hover:shadow-lg">
       <Image
         width={400}
         height={400}
-        className=" w-full max-h-80 h-full object-cover object-center"
+        className=" w-full max-h-80 object-cover object-center"
         src="https://images.unsplash.com/photo-1674296115670-8f0e92b1fddb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
         alt="Product Image"
       />
