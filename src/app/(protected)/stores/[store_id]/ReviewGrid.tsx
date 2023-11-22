7d00665d8f4c74be5ddb602629a27899bc29171c
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-11-22 13:58:58 +0900

    todo Fix review query

--- src/app/(protected)/stores/[store_id]/ReviewGrid.tsx
+++ src/app/(protected)/stores/[store_id]/ReviewGrid.tsx
@@ -1,25 +1,32 @@
 import Image from "next/image";
+import { getReviewsWithImages2 } from "@/utils/reviews";
 function ReviewGridItem() {
   return (
-    <div className='mx-auto  max-w-sm w-full transform overflow-hidden  bg-white shadow-md duration-300 hover:scale-105 hover:shadow-lg'>
+    <div className="mx-auto  max-w-sm w-full transform overflow-hidden  bg-white shadow-md duration-300 hover:scale-105 hover:shadow-lg">
       <Image
         width={400}
         height={400}
-        className=' w-full max-h-80 object-cover object-center'
-        src='https://images.unsplash.com/photo-1674296115670-8f0e92b1fddb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
-        alt='Product Image'
+        className=" w-full max-h-80 object-cover object-center"
+        src="https://images.unsplash.com/photo-1674296115670-8f0e92b1fddb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+        alt="Product Image"
       />
     </div>
   );
 }
 
-function ReviewGrid() {
+async function ReviewGrid({ store_id }: { store_id: string }) {
   // fetch data here
-  const reviewsList = Array.from({ length: 6 }, (_, index) => <ReviewGridItem key={index} />);
+  const res = await getReviewsWithImages2(store_id);
+  console.log(res);
+  const reviewsList = Array.from({ length: 6 }, (_, index) => (
+    <ReviewGridItem key={index} />
+  ));
 
   return (
     <>
-      <div className='grid grid-cols-3 gap-0 mx-auto max-w-3xl px-1 '>{reviewsList}</div>
+      <div className="grid grid-cols-3 gap-0 mx-auto max-w-3xl px-1 ">
+        {reviewsList}
+      </div>
     </>
   );
 }
--- src/app/(protected)/stores/[store_id]/page.tsx
+++ src/app/(protected)/stores/[store_id]/page.tsx
@@ -4,9 +4,11 @@ import ReviewGrid from "./ReviewGrid";
 export default function Page({ params }: { params: { store_id: string } }) {
   const { store_id } = params;
   return (
-    <section className='overflow-y-scroll h-screen overscroll-none'>
-      <HeaderCard_2 data={fetch(`http://localhost:3000/api/stores/${store_id}`)} />
-      <ReviewGrid />
+    <section className="overflow-y-scroll h-screen overscroll-none">
+      <HeaderCard_2
+        data={fetch(`http://localhost:3000/api/stores/${store_id}`)}
+      />
+      <ReviewGrid store_id={store_id} />
       <Reviews store_id={store_id} />
     </section>
   );
