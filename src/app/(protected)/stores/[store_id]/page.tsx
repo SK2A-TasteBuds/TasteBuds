3616f6bab3d1ac7218c10f8b0b597fe1724d53dd
Dragon00716 <pmg76dayo@gmail.com>
2023-11-28 12:17:07 +0900

    session user機能

--- src/app/(protected)/stores/[store_id]/page.tsx
+++ src/app/(protected)/stores/[store_id]/page.tsx
@@ -1,6 +1,7 @@
 import HeaderCard from './HeaderCard';
 import Reviews from './Reviews';
 import ReviewGrid from './ReviewGrid';
+import Link from 'next/link';
 
 type PageProps = {
   params: { store_id: string };
@@ -9,12 +10,15 @@ type PageProps = {
 export default function Page({ params }: PageProps) {
   const { store_id } = params;
   return (
-    <section className="overflow-y-scroll h-screen overscroll-none">
+    <section className="overflow-y-scroll h-screen overscroll-none pb-20">
       <HeaderCard
         data={fetch(`http://localhost:3000/api/stores/${store_id}`)}
       />
+      <Link href={`/stores/${store_id}/postReview`}>
+        <p className='text-white border-black border bg-orange-500  text-center m-5'>レビューを投稿する</p>  
+      </Link> 
       <ReviewGrid store_id={store_id} />
-      <Reviews store_id={store_id} />
+      <Reviews store_id={store_id} /> 
     </section>
   );
 }
