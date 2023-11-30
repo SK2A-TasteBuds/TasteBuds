73227ecc1b309573ca4392b0ed760d6f8e168745
Dragon00716 <pmg76dayo@gmail.com>
2023-11-30 12:10:36 +0900

    デザイン修正、アイコン変更、svg追加

--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -5,6 +5,9 @@ import { useGeolocation } from "@/contexts/GeolocationProvider";
 import { useSession } from "next-auth/react";
 import SignOutBtn from "../components/SignOutBtn";
 import { useEffect, useState } from "react";
+import Image from "next/image"
+import thumbs_up from "@/assets/svg/thumbs-up.svg"
+import thumbs_down from "@/assets/svg/thumbs-down.svg"
 
 
 
@@ -108,39 +111,41 @@ export default function Main() {
 
       <div className='store-img relative h-[75%] w-full  overflow-hidden' id='store-img'>
         <div className='swipe flex '>
-          <div className='object-cover block h-full w-[20px] bg-[#ffc7b7] rounded-md absolute z-10 left-[90%]'></div>
-          <div className='object-cover block h-full w-[20px] bg-[#fe9477] rounded-md absolute z-10 left-[88%]'></div>
-          <img
-
+          <div className='object-cover block h-full w-[40px] bg-[#ffc7b7] rounded-md absolute z-10 left-[90%]'></div>
+          <div className='object-cover block h-full w-[38px] bg-[#fe9477] rounded-md absolute z-10 left-[88%]'></div>
+          <Image
             src={store.img}
             alt='店舗画像'
-            className='object-cover w-[90%] h-full absolute z-10 rounded-md'
+            className='w-[95%] h-full absolute z-10 rounded-md max-w-2xl'
             id="show-img"
             loading="lazy"
+            width={400}
+            height={400}
           />
 
-
         </div>
 
-        <div className='absolute bottom-20 left-10  p-2 bg-transparent z-20'>
+        <div className='absolute bottom-2 left-4  p-2 bg-transparent z-20'>
           <p className='bg-transparent text-white'>{store.name}</p>
-          <p className='bg-transparent text-white'>{store.address}</p>
         </div>
+
+
       </div>
 
 
       <div className="good-bad w-full h-1/5 flex items-center mt-3">
         <div className="object-cover w-full h-full flex justify-center">
-          <div className="bad rounded-full bg-gray-400 object-cover w-2/6 h-2/5 justify-center items-center flex">
-            <button className="flex" onClick={() => nextStore(showIndex+1)}>
-              <img src="https://www.svgrepo.com/show/449596/thumbs-down.svg" alt="" className="bg-transparent object-cover w-full h-full m-auto" />
+          <div className="bad rounded-full bg-transparent object-cover w-2/6 h-2/5 justify-center items-center flex">
+            <button className="flex shadow-lg px-2 py-1  bg-transparent text-lg text-white font-semibold rounded-full bg-gradient-to-t from-transparent to-blue-100" onClick={() => nextStore(showIndex+1)}>
+              <Image src={thumbs_down} alt="" className="bg-transparent object-cover w-full h-full m-auto"/>
             </button>
           </div>
         </div>
         <div className="object-cover w-full h-full flex justify-center">
-          <div className="good rounded-full bg-gray-400 object-cover w-2/6 h-2/5 justify-center items-center flex">
-            <button className="flex" onClick={() => nextStore(showIndex+1)}>
-              <img src="https://www.svgrepo.com/show/449597/thumbs-up.svg" alt="" className="bg-transparent object-cover w-full h-full m-auto" />
+          <div className="good rounded-full bg-transparent object-cover w-2/6 h-2/5 justify-center items-center flex">
+            <button className="flex shadow-lg px-2 py-1  bg-transparent text-lg text-white font-semibold rounded-full bg-gradient-to-t from-transparent to-red-100" onClick={() => nextStore(showIndex+1)}>
+              <Image src={thumbs_up} alt="" className="bg-transparent object-cover w-full h-full m-auto"/>
+             
 
             </button>
           </div>
