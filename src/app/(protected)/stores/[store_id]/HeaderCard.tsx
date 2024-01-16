370819df22e1f401bd217caeb3383801f4ed34bf
Eakudompong Chanoknan <ceguantz96@outlook.com>
2024-01-16 11:09:05 +0900

    add badge functionality

--- src/app/(protected)/stores/[store_id]/HeaderCard.tsx
+++ src/app/(protected)/stores/[store_id]/HeaderCard.tsx
@@ -1,6 +1,16 @@
 import Image from 'next/image';
-
-async function HeaderCard({ data }: { data: Promise<Response> }) {
+import HotBadge from '@/app/components/badges/HotBadge';
+import LikeBadge from '@/app/components/badges/LikeBadge';
+LikeBadge;
+async function HeaderCard({
+  data,
+  ratio,
+  reviewCount,
+}: {
+  data: Promise<Response>;
+  ratio: number;
+  reviewCount: number;
+}) {
   const store = await data.then((res) => res.json());
   return (
     <div className="mx-auto mt-4 max-w-md w-full md:max-w-3xl md:h-fit md:flex sticky -top-[265px] md:-top-[500px] md:mb-10  shadow-md z-10">
@@ -14,19 +24,10 @@ async function HeaderCard({ data }: { data: Promise<Response> }) {
       />
       <div className="p-4 max-w-xl w-full">
         <div className="flex justify-between ">
-          <h3 className="text-xs">{store.genre.name}</h3>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-6 h-6 fill-orange-500"
-          >
-            <path
-              fillRule="evenodd"
-              d="M12.963 2.286a.75.75 0 00-1.071-.136 9.742 9.742 0 00-3.539 6.177A7.547 7.547 0 016.648 6.61a.75.75 0 00-1.152-.082A9 9 0 1015.68 4.534a7.46 7.46 0 01-2.717-2.248zM15.75 14.25a3.75 3.75 0 11-7.313-1.172c.628.465 1.35.81 2.133 1a5.99 5.99 0 011.925-3.545 3.75 3.75 0 013.255 3.717z"
-              clipRule="evenodd"
-            />
-          </svg>
+          <h3 className="text-xs h-6">{store.genre.name}</h3>
+          {reviewCount >= 10 ? <HotBadge /> : null}
+
+          {ratio >= 80 ? <LikeBadge /> : null}
         </div>
         <h2 className=" text-lg font-bold text-gray-900 ">{store.name}</h2>
         <p className=" text-sm text-gray-700 py-1">{store.catch}</p>
