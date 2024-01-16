50b24fb6f9c7911a1ead2669ee31bae5712e2c97
Eakudompong Chanoknan <ceguantz96@outlook.com>
2024-01-16 11:15:11 +0900

    fix typo

--- src/app/(protected)/map/[user_id]/page.tsx
+++ src/app/(protected)/map/[user_id]/page.tsx
@@ -2,17 +2,12 @@
 
 import { useGeolocation } from '@/contexts/GeolocationProvider';
 import { useSession } from 'next-auth/react';
-
 import { useEffect, useState, useRef } from 'react';
-import mapboxgl, { GeoJSONSource, MapMouseEvent, Marker } from 'mapbox-gl';
-import Image from 'next/image';
-
+import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './svg.css';
-
 import { getUserKeepStore, getUserLikesStore } from '@/utils/stores';
 import { getUser } from '@/utils/user';
-import { Qahiri } from 'next/font/google';
 
 type PageProps = {
   params: { user_id: string };
@@ -29,27 +24,24 @@ function Map({ params }: PageProps) {
   const [lat, setLat] = useState(42.35);
 
   const [zoom, setZoom] = useState(4);
-  const LikeListColor = "#fb923c";
-  const KeepListColor = "#ffff00";
-  
+  const LikeListColor = '#fb923c';
+  const KeepListColor = '#ffff00';
+
   //ユーザー情報取得
-  const [user_data,setUserData] = useState({name:"null",image:"null"});
-  const getUserParams = async ()=>{
+  const [user_data, setUserData] = useState({ name: 'null', image: 'null' });
+  const getUserParams = async () => {
     return await getUser(user_id);
-  }
-  useEffect(()=>{
-    if(user_data.name == 'null'){
-      getUserParams().then((e)=>{
+  };
+  useEffect(() => {
+    if (user_data.name == 'null') {
+      getUserParams().then((e) => {
         const name = e?.name;
         const image = e?.image;
-        setUserData({name:name,image:image})
+        setUserData({ name: name, image: image });
       });
-      console.log(user_data)
+      console.log(user_data);
     }
-  },[user_data])
- 
-
-
+  }, [user_data]);
 
   useEffect(() => {
     // Get user's current lng lat
@@ -98,62 +90,70 @@ function Map({ params }: PageProps) {
   }, [location]); // Empty dependency array ensures this effect runs only once
 
   useEffect(() => {
-    if (user_id != null && map.current != null) {
-      const getKeepList = async () => {
-        return await getUserKeepStore(user_id);
+    const fetchData = async () => {
+      if (user_id != null && map.current != null) {
+        try {
+          const getKeepList = async () => {
+            return await getUserKeepStore(user_id);
+          };
 
+          const getLikeList = async () => {
+            return await getUserLikesStore(user_id);
+          };
+
+          const keepMarkers = await getKeepList();
+          keepMarkers.forEach((element) => {
+            if (map.current != null) {
+              const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(`
+                <div>
+                  <a href=/stores/${element['id']}>
+                    <img src=${element['photo']} alt="store-img">
+                    ${element['name']}
+                  </a>  
+                </div>  
+              `);
+              const pin = new mapboxgl.Marker({ color: KeepListColor })
+                .setLngLat([element['lng'], element['lat']])
+                .setPopup(popup)
+                .addTo(map.current);
+            }
+          });
+
+          const likeMarkers = await getLikeList();
+          likeMarkers.forEach((element) => {
+            if (map.current != null) {
+              const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(`
+                <div>
+                  <a href=/stores/${element['id']}>
+                    <img src=${element['photo']} alt="store-img">
+                    ${element['name']}
+                  </a> 
+                </div>  
+              `);
+              const pin = new mapboxgl.Marker({ color: LikeListColor })
+                .setLngLat([element['lng'], element['lat']])
+                .setPopup(popup)
+                .addTo(map.current);
+            }
+          });
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
       }
-      };
+    };
 
-      const getLikeList = async () => {
-        return await getUserLikesStore(user_id);
-      };
-      const KeepMarkers = getKeepList();
-      KeepMarkers.then((data) => {
-        data.forEach((element) => {
-          console.log('Processing element:', element); // デバッグログ
-          if (map.current != null) {
-            const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(`
-                        <div>
-                          <a href=/stores/${element['id']}>
-                            <img src=${element['photo']} alt="store-img">
-                            ${element['name']}
-                          </a>  
-                        </div>  
-                      `);
-            const pin = new mapboxgl.Marker({ color: KeepListColor })
-              .setLngLat([element['lng'], element['lat']])
-              .setPopup(popup)
-              .addTo(map.current);
-          }
-        });
-      });
-      const LikeMarkers = getLikeList();
-
-      LikeMarkers.then((data) => {
-        data.forEach((element) => {
-          if (map.current != null) {
-            const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(`
-                        <div>
-                          <a href=/stores/${element['id']}>
-                            <img src=${element['photo']} alt="store-img">
-                            ${element['name']}
-                          </a> 
-                        </div>  
-                      `);
-            const pin = new mapboxgl.Marker({ color: LikeListColor })
-              .setLngLat([element['lng'], element['lat']])
-              .setPopup(popup)
-              .addTo(map.current);
-          }
-        });
-      });
-    }
-  }, [map.current]);
+    fetchData();
+  }, [map.current, user_id]);
   return (
     <div className="h-screen">
-      <div className='flex'>
-        <img src={`${user_data.image}`} alt={`${user_data.name}`} width={120} height={120} className="mr-2 w-6 h-6 rounded-full"/>
+      <div className="flex">
+        <img
+          src={`${user_data.image}`}
+          alt={`${user_data.name}`}
+          width={120}
+          height={120}
+          className="mr-2 w-6 h-6 rounded-full"
+        />
         {user_data.name}
       </div>
 
