14eadb0a57c72656481c3ae3a1de812e24918d08
Eakudompong Chanoknan <ceguantz96@outlook.com>
2024-01-18 09:31:01 +0900

    design

--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -106,16 +106,16 @@ export default function Main(request: any) {
     }
   };
   return (
-    <div className='flex flex-col overflow-hidden items-center justify-start w-full h-screen'>
-      <Header name='Home' />
+    <div className="flex flex-col overflow-hidden items-center justify-start w-full h-screen">
+      <Header name="Home" />
       <MainFilter />
 
       {data === null ? (
         <MainSkeleton />
       ) : (
         <div
-          style={{ height: '480px' }}
-          className='relative max-w-md w-full overflow-hidden flex justify-center mt-5  p-4 my-2 '
+          style={{ height: '428px' }}
+          className="relative max-w-md w-full overflow-hidden flex justify-center mt-5  p-4 my-2 "
         >
           {data
             ?.slice()
@@ -130,71 +130,77 @@ export default function Main(request: any) {
               >
                 <Image
                   src={store.photo}
-                  alt='store_img'
-                  className='h-72 w-full rounded-xl md:h-72 lg:h-80 '
+                  alt="store_img"
+                  className="h-72 w-full rounded-xl md:h-72 lg:h-80 "
                   width={320}
                   height={320}
                   priority
                 />
 
-                <div className='py-4 max-w-md w-full'>
-                  <div className='flex justify-between '>
-                    <h3 className='text-xs'>{store.genre?.name}</h3>
+                <div className="py-4 max-w-md w-full">
+                  <div className="flex justify-between ">
+                    <h3 className="text-xs">{store.genre?.name}</h3>
                     <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      viewBox='0 0 24 24'
-                      fill='currentColor'
-                      className='w-6 h-6 fill-orange-500'
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="w-6 h-6 fill-orange-500"
                     >
                       {/* SVG path here */}
                     </svg>
                   </div>
-                  <p className='mb-2 text-xl font-bold tracking-tight text-gray-900 '>
+                  <p className="mb-2 text-xl font-bold tracking-tight text-gray-900 ">
                     {store.name}
                   </p>
-                  <p className='mb-3 text-xs font-normal text-gray-700 '>{store.address}</p>
+                  <p className="mb-3 text-xs font-normal text-gray-700 ">
+                    {store.address}
+                  </p>
                 </div>
               </TinderCard>
             ))}
         </div>
       )}
 
-      <div className='flex items-center justify-around max-w-md w-full  pt-8'>
+      <div className="flex items-center justify-around max-w-md w-full  ">
         <button
-          className='rounded-full'
+          className="rounded-full"
           onClick={() => {
             onSwipe('left');
           }}
         >
           <svg
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
             strokeWidth={1.5}
-            stroke='currentColor'
-            className='w-14 h-14 rounded-full bg-zinc-400 text-white'
+            stroke="currentColor"
+            className="w-14 h-14 rounded-full bg-zinc-400 text-white"
           >
-            <path strokeLinecap='round' strokeLinejoin='round' d='M6 18L18 6M6 6l12 12' />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
           </svg>
         </button>
         <button
-          className='rounded-full'
+          className="rounded-full"
           onClick={() => {
             onSwipe('right');
           }}
         >
           <svg
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
             strokeWidth={1.5}
-            stroke='currentColor'
-            className='w-14 h-14 rounded-full bg-yellow-400 text-white p-1'
+            stroke="currentColor"
+            className="w-14 h-14 rounded-full bg-yellow-400 text-white p-1"
           >
             <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              d='M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z'
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
             />
           </svg>
         </button>
