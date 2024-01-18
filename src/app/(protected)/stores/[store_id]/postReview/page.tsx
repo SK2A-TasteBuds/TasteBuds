662561af692f726932ac23f0878653a1ea35c1df
Eakudompong Chanoknan <ceguantz96@outlook.com>
2024-01-18 11:38:26 +0900

    change postReview Page to Home

--- src/app/(protected)/stores/[store_id]/postReview/page.tsx
+++ src/app/(protected)/stores/[store_id]/postReview/page.tsx
@@ -7,7 +7,7 @@ import { addToLikes, removeFromKeeps } from '@/utils/user';
 import { reviewData } from '@/types/types';
 import { useRouter } from 'next/navigation';
 
-export function Page({ params }: { params: { store_id: string } }) {
+export function Home({ params }: { params: { store_id: string } }) {
   const router = useRouter();
   const { store_id } = params;
   const { data } = useSession();
@@ -76,22 +76,22 @@ export function Page({ params }: { params: { store_id: string } }) {
   };
 
   return (
-    <div className='flex min-h-full  flex-col justify-center items-start px-6 py-12 lg:px-8 '>
-      <div className='text-center'>{store_name}</div>
-      <p className='inline-flex items-center mr-3 py-2 text-sm text-gray-900  font-semibold'>
+    <div className="flex min-h-full  flex-col justify-center items-start px-6 py-12 lg:px-8 ">
+      <div className="text-center">{store_name}</div>
+      <p className="inline-flex items-center mr-3 py-2 text-sm text-gray-900  font-semibold">
         <Image
           width={120}
           height={120}
-          className='mr-2 w-6 h-6 rounded-full'
+          className="mr-2 w-6 h-6 rounded-full"
           src={data?.user.image}
-          alt='User Image'
+          alt="User Image"
         />
         {data?.user.name}
       </p>
       <form>
-        <div className='flex max-w-3xl w-full py-2'>
+        <div className="flex max-w-3xl w-full py-2">
           <button
-            type='button'
+            type="button"
             className={`w-1/2  border-b-4 ${
               toggleLike === 'like' ? 'border-orange-400' : 'border-none'
             } `}
@@ -100,7 +100,7 @@ export function Page({ params }: { params: { store_id: string } }) {
             like
           </button>
           <button
-            type='button'
+            type="button"
             className={`w-1/2  border-b-4 ${
               toggleLike === 'dislike' ? 'border-orange-400' : 'border-none'
             }`}
@@ -114,19 +114,19 @@ export function Page({ params }: { params: { store_id: string } }) {
           value={reviewContent}
           onChange={(e) => setReviewContent(e.target.value)}
         />
-        <div className='flex flex-col items-start justify-start py-2'>
+        <div className="flex flex-col items-start justify-start py-2">
           <input
-            type='file'
-            accept='image/*'
+            type="file"
+            accept="image/*"
             onChange={onFileInputChange}
             ref={fileInputRef}
-            className=''
+            className=""
           />
         </div>
-        <div className='text-center'>
+        <div className="text-center">
           <button
-            type='submit'
-            className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'
+            type="submit"
+            className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
             onClick={handleSubmit}
           >
             投稿する
@@ -142,18 +142,21 @@ interface MyTextareaProps {
   onChange: (e: ChangeEvent<HTMLTextAreaElement>) => void;
 }
 
-const MyTextareaComponent: React.FC<MyTextareaProps> = ({ value, onChange }) => {
+const MyTextareaComponent: React.FC<MyTextareaProps> = ({
+  value,
+  onChange,
+}) => {
   return (
     <textarea
-      name='review'
-      id='review'
+      name="review"
+      id="review"
       cols={43}
       rows={8}
-      className='border border-gray-500 rounded-lg'
+      className="border border-gray-500 rounded-lg"
       value={value}
       onChange={onChange}
     ></textarea>
   );
 };
 
-export default Page;
+export default Home;
