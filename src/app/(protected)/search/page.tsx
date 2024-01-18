7bec6153343d62d50388e0125f8c9b3d560bc2b1
Eakudompong Chanoknan <ceguantz96@outlook.com>
2024-01-18 12:03:16 +0900

    add type to search header style

--- src/app/(protected)/search/page.tsx
+++ src/app/(protected)/search/page.tsx
@@ -2,10 +2,10 @@
 import React, { useState, useEffect } from 'react';
 import FoodCard from './FoodCard';
 import Header from '@/app/components/HeaderBar';
-import Main from "../page";
+import Main from '../page';
 import Image from 'next/image';
 import SushiSvg from '@/assets/svg/sushi.svg';
-import FruitSandwichSvg from '@/assets/svg/fruit-sandwich.svg'
+import FruitSandwichSvg from '@/assets/svg/fruit-sandwich.svg';
 import Link from 'next/link';
 
 export default function Home() {
@@ -26,12 +26,44 @@ export default function Home() {
   }, []); // 空の依存配列で効果はコンポーネントのマウント時にのみ実行されます
 
   // 特集ページのCSS
-  const bestcontainerStyle = {
+
+  type CircleStyle = {
+    width: string;
+    height: string;
+    borderRadius: string;
+    WebkitBorderRadius: string;
+    MozBorderRadius: string;
+    background: string;
+    position: 'absolute';
+    top: string;
+    left: string;
+  };
+
+  type TextStyle = {
+    textAlign: string;
+    background: string;
+    fontSize: string;
+    fontWeight: string;
+    color: string;
+    position: 'absolute';
+    top: string;
+    left: string;
+    whiteSpace: string;
+  };
+
+  type ContainerStyle = {
+    height: string;
+    width: string;
+    background: string;
+  };
+
+  const bestcontainerStyle: ContainerStyle = {
     height: '223px',
     width: '52%',
-    background: 'linear-gradient(90deg, #FFC7B7 0% 60%, #FFAC95 60% 80%, #FE724C 80% 100%)',
+    background:
+      'linear-gradient(90deg, #FFC7B7 0% 60%, #FFAC95 60% 80%, #FE724C 80% 100%)',
   };
-  const bestcirclestyle1 = {
+  const bestcirclestyle1: CircleStyle = {
     width: '200px',
     height: '200px',
     borderRadius: '50%',
@@ -42,7 +74,7 @@ export default function Home() {
     top: '15px',
     left: '110px',
   };
-  const bestcirclestyle2 = {
+  const bestcirclestyle2: CircleStyle = {
     width: '190px',
     height: '190px',
     borderRadius: '50%',
@@ -53,7 +85,7 @@ export default function Home() {
     top: '20px',
     left: '115px',
   };
-  const bestcirclestyle3 = {
+  const bestcirclestyle3: CircleStyle = {
     width: '180px',
     height: '180px',
     borderRadius: '50%',
@@ -64,26 +96,27 @@ export default function Home() {
     top: '25px',
     left: '120px',
   };
-  const besttextstyle = {
+  const besttextstyle: any = {
     textAlign: 'center', // 'left', 'right', 'justify' なども選択可能
     background: '#FFF',
-    fontSize: '2.0rem',   // フォントサイズの指定
+    fontSize: '2.0rem', // フォントサイズの指定
     // fontFamily: 'Arial',
-    fontWeight: 'bold',   // フォントの太さ
-    color: '#FF0F00',     // テキストの色
+    fontWeight: 'bold', // フォントの太さ
+    color: '#FF0F00', // テキストの色
     position: 'absolute',
     top: '60px',
     left: '165px',
     whiteSpace: 'pre-line',
-  }
-
-  const hotcontainerStyle = {
-    height: '223px',
-    width: '52%',
-    background: 'linear-gradient(90deg, #FFFCB5 0% 60%, #FFFA77 60% 80%, #F9FE1B 80% 100%)',
   };
 
-  const hotcirclestyle1 = {
+  const hotcontainerStyle: ContainerStyle = {
+    height: '223px',
+    width: '52%',
+    background:
+      'linear-gradient(90deg, #FFFCB5 0% 60%, #FFFA77 60% 80%, #F9FE1B 80% 100%)',
+  };
+
+  const hotcirclestyle1: CircleStyle = {
     width: '200px',
     height: '200px',
     borderRadius: '50%',
@@ -94,7 +127,7 @@ export default function Home() {
     top: '15px',
     left: '110px',
   };
-  const hotcirclestyle2 = {
+  const hotcirclestyle2: CircleStyle = {
     width: '190px',
     height: '190px',
     borderRadius: '50%',
@@ -105,7 +138,7 @@ export default function Home() {
     top: '20px',
     left: '115px',
   };
-  const hotcirclestyle3 = {
+  const hotcirclestyle3: CircleStyle = {
     width: '180px',
     height: '180px',
     borderRadius: '50%',
@@ -116,24 +149,24 @@ export default function Home() {
     top: '25px',
     left: '120px',
   };
-  const hottextstyle = {
+  const hottextstyle: any = {
     textAlign: 'center', // 'left', 'right', 'justify' なども選択可能
     background: '#FFF',
-    fontSize: '2.0rem',   // フォントサイズの指定
+    fontSize: '2.0rem', // フォントサイズの指定
     // fontFamily: 'Noto Sans sans-serif',
-    fontWeight: 'bold',   // フォントの太さ
-    color: '#FFF500',     // テキストの色
+    fontWeight: 'bold', // フォントの太さ
+    color: '#FFF500', // テキストの色
     position: 'absolute',
     top: '60px',
     left: '165px',
     whiteSpace: 'pre-line',
-  }
+  };
 
   const meatstyle = {
     position: 'absolute',
     top: '110px',
     left: '170px',
-  }
+  };
 
   const [currentSlide, setCurrentSlide] = useState(0);
 
@@ -142,77 +175,135 @@ export default function Home() {
     setCurrentSlide((prev) => (prev + 1) % 2); // 例: スライドが2つの場合
   };
   const handleMain = () => {
-
-    <Link href="/"></Link>
-
-  }
+    <Link href="/"></Link>;
+  };
   // コンポーネントのUI部分
 
   return (
     <>
-
       <Header name="Search" />
       {/* 特集ページ */}
 
-      
-        {currentSlide === 0 && (
-          // スライド1のコンテンツ
-          
-          <div className='flex ml-2 items-center relative'>
-            <button onClick={handleNextSlide} className="absolute top-24 bg-transparent">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="gray" className="w-10 h-10 bg-transparent">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
-              </svg>
-            </button>
-            <button onClick={handleNextSlide} className=" absolute top-24 right-0 bg-transparent">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="gray" className="w-10 h-10 bg-transparent">
-                <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-              </svg>
-            </button>
-            <a href="/">
-              <Image src={SushiSvg} alt='sushi' width={175} />
-            </a>
-            <a href='/' style={bestcontainerStyle}>
-              <div style={bestcirclestyle1}></div>
-              <div style={bestcirclestyle2}></div>
-              <div style={bestcirclestyle3}></div>
-              <div style={besttextstyle}>BEST{'\n'}STORE</div>
-            </a>
-            
-          </div>
-          
-          
-        )}
-        {currentSlide === 1 && (
-          // スライド2のコンテンツ
-          <div className='flex ml-2 items-center relative'>
-            {/* back */}
-            <button onClick={handleNextSlide} className="absolute top-24 bg-transparent">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="gray" className="w-10 h-10 bg-transparent">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
-              </svg>
-            </button>
-            {/* next */}
-            <button onClick={handleNextSlide} className=" absolute top-24 right-0 bg-transparent">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="gray" className="w-10 h-10 bg-transparent">
-                <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-              </svg>
-            </button>
-            <a href="/">
-              <Image onClick={handleMain} src={FruitSandwichSvg} alt='sushi' width={175}/>
-            </a>
-            <a href='/'style={hotcontainerStyle}>
-              <div style={hotcirclestyle1}></div>
-              <div style={hotcirclestyle2}></div>
-              <div style={hotcirclestyle3}></div>
-              <div style={hottextstyle}>HOT{'\n'}STORE</div>
-            </a>
-          </div>
-        )}
+      {currentSlide === 0 && (
+        // スライド1のコンテンツ
+
+        <div className="flex ml-2 items-center relative">
+          <button
+            onClick={handleNextSlide}
+            className="absolute top-24 bg-transparent"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="gray"
+              className="w-10 h-10 bg-transparent"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15.75 19.5 8.25 12l7.5-7.5"
+              />
+            </svg>
+          </button>
+          <button
+            onClick={handleNextSlide}
+            className=" absolute top-24 right-0 bg-transparent"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="gray"
+              className="w-10 h-10 bg-transparent"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m8.25 4.5 7.5 7.5-7.5 7.5"
+              />
+            </svg>
+          </button>
+          <a href="/">
+            <Image src={SushiSvg} alt="sushi" width={175} />
+          </a>
+          <a href="/" style={bestcontainerStyle}>
+            <div style={bestcirclestyle1}></div>
+            <div style={bestcirclestyle2}></div>
+            <div style={bestcirclestyle3}></div>
+            <div style={besttextstyle}>BEST{'\n'}STORE</div>
+          </a>
+        </div>
+      )}
+      {currentSlide === 1 && (
+        // スライド2のコンテンツ
+        <div className="flex ml-2 items-center relative">
+          {/* back */}
+          <button
+            onClick={handleNextSlide}
+            className="absolute top-24 bg-transparent"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="gray"
+              className="w-10 h-10 bg-transparent"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15.75 19.5 8.25 12l7.5-7.5"
+              />
+            </svg>
+          </button>
+          {/* next */}
+          <button
+            onClick={handleNextSlide}
+            className=" absolute top-24 right-0 bg-transparent"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="gray"
+              className="w-10 h-10 bg-transparent"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m8.25 4.5 7.5 7.5-7.5 7.5"
+              />
+            </svg>
+          </button>
+          <a href="/">
+            <Image
+              onClick={handleMain}
+              src={FruitSandwichSvg}
+              alt="sushi"
+              width={175}
+            />
+          </a>
+          <a href="/" style={hotcontainerStyle}>
+            <div style={hotcirclestyle1}></div>
+            <div style={hotcirclestyle2}></div>
+            <div style={hotcirclestyle3}></div>
+            <div style={hottextstyle}>HOT{'\n'}STORE</div>
+          </a>
+        </div>
+      )}
 
       {/* フォーム追加 */}
-      <form action="" className='ml-2 mt-1 mb-1'>
-        <input type="text" className='border-2 border-black' placeholder="エリア検索"/>
+      <form action="" className="ml-2 mt-1 mb-1">
+        <input
+          type="text"
+          className="border-2 border-black"
+          placeholder="エリア検索"
+        />
       </form>
 
       <div className="shadow-md rounded-lg overflow-hidden flex items-center justify-center px-2 pb-20 ">
@@ -225,7 +316,6 @@ export default function Home() {
               genre_code={item.genre_code}
             />
           ))}
-
         </div>
       </div>
     </>
